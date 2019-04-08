58f1e1d620a03fa14910307544cc634967d2230c
Anton <40821074+AntonNaumchik@users.noreply.github.com>
2019-04-08 16:53:17 +0300

    Fix code: add additional method,edit method task_2

--- HW_03/Anton_Naumchuk/hw_03_t_02.rb
+++ HW_03/Anton_Naumchuk/hw_03_t_02.rb
@@ -1,13 +1,24 @@
-# rubocop:disable Metrics/AbcSize
+ADDRESS_REGEXP = /[A-Z]{4}.[0-9].[A-Z]{1,8}/.freeze
+DATE_REGEXP = %r{\w{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\D+\d{4}}.freeze
+IP_REGEXP = /\d+.\d+.\d+.\d+/.freeze
+
 def task_2(str)
   str.split("\n")
-  a = str.upcase.scan(/[A-Z]{4}.[0-9].[A-Z]{1,8}/)
-  d = str.scan(%r{\w{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\D+\d{4}})
-  i = str.scan(/\d+.\d+.\d+.\d+/)
-  b = "#{d[0]} FROM: #{i[0]} TO: /#{a[0]}"
-  c = "#{d[1]} FROM: #{i[3]} TO: /#{a[1]}"
-  w = "#{d[2]} FROM: #{i[3]} TO: /#{a[2]}"
-  pp = [b, c, w]
-  a.to_s.length < 5 ? '' : pp
+  addr = str.upcase.scan(ADDRESS_REGEXP)
+  date = str.scan(DATE_REGEXP)
+  ip = str.scan(IP_REGEXP)
+  pp = str_1(date, addr, ip), str_2(date, addr, ip), str_3(date, addr, ip)
+  addr.to_s.length < 5 ? '' : pp
 end
-# rubocop:enable Metrics/AbcSize
+
+def str_1(date, addr, ip)
+  "#{date[0]} FROM: #{ip[0]} TO: /#{addr[0]}"
+end
+
+def str_2(date, addr, ip)
+  "#{date[1]} FROM: #{ip[3]} TO: /#{addr[1]}"
+end
+
+def str_3(date, addr, ip)
+  "#{date[2]} FROM: #{ip[3]} TO: /#{addr[2]}"
+end
